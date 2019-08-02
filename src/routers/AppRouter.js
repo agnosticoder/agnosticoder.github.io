78872cc1fc23ff810482a4b33439d27df214ca42
agnosticoder <agnosticoder@gmail.com>
2019-08-01 22:48:31 -0400

    Setup react-router and components

---
+++ src/routers/AppRouter.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import {BrowserRouter, Route, Switch} from 'react-router-dom';
+import Home from '../components/Home';
+import Projects from '../components/Projects';
+import Goals from '../components/Goals';
+import About from '../components/About';
+import Blogs from '../components/Blogs';
+import Contact from '../components/Contact';
+import NotFoundPage from '../components/NotFoundPage';
+import Header from '../components/Header';
+
+
+const AppRouter = () => {
+    return (
+        <div>
+            <BrowserRouter>
+                <Header />
+                <Switch>
+                    <Route path='/' component={Home} exact={true} />
+                    <Route path='/projects' component={Projects} />
+                    <Route path='/goals' component={Goals} />
+                    <Route path='/about' component={About} />
+                    <Route path='/blogs' component={Blogs} />
+                    <Route path='/contact' component={Contact} />
+                    <Route component={NotFoundPage} />
+                </Switch>
+            </BrowserRouter>
+        </div>
+    );
+};
+
+export default AppRouter;
