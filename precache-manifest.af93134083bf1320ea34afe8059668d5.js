34aac64d58cb5ddfe9cfc5de249ee38897bbe99b
agnosticoder <agnosticoder@gmail.com>
2020-04-18 19:39:48 -0400

    Updates

---
+++ precache-manifest.af93134083bf1320ea34afe8059668d5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1d5f799703b218e524e2ea16d18025a6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "769448b7cf2c25977871",
+    "url": "/static/css/main.ab52de60.chunk.css"
+  },
+  {
+    "revision": "a977275db837a5132771",
+    "url": "/static/js/2.f225d79a.chunk.js"
+  },
+  {
+    "revision": "769448b7cf2c25977871",
+    "url": "/static/js/main.f349da14.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
